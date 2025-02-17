5ce34d52f3cef05900427c9f266a3e9da75ee654
Jill Chang <jill20021@gmail.com>
2025-02-17 17:00:47 +0800

    build: Import DemoSDK

---
+++ TUTKDemo/TUTKDemo-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  TUTKDemo-Bridging-Header.h
+//  TUTKDemo
+//
+//  Created by Jill Chang on 2025/2/17.
+//
+
+#ifndef TUTKDemo_Bridging_Header_h
+#define TUTKDemo_Bridging_Header_h
+
+
+#endif /* TUTKDemo_Bridging_Header_h */
+#import "DemoSDK.h"
