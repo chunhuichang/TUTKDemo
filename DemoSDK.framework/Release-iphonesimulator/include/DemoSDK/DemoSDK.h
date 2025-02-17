60d0bb8088651802996155d566fd5af9c79afddb
Jill Chang <jill20021@gmail.com>
2025-02-17 16:58:56 +0800

    chore: Add DemoSDK framework

---
+++ DemoSDK.framework/Release-iphonesimulator/include/DemoSDK/DemoSDK.h
@@ -0,0 +1,12 @@
+//
+//  DemoSDK.h
+//  DemoSDK
+//
+//  Created by Jill Chang on 2025/2/15.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DemoSDK : NSObject
++ (NSString *)getVersion;
+@end
